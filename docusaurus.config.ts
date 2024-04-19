7def82c7b8a201de8523a3c4c02841e755b53546
space/acc <info@spaceacc.com>
2024-04-19 18:42:41 -0500

    correct edit urls and broken link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/spaceacc/spaceacc.github.io',
+            'https://github.com/spaceacc/spaceacc.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/spaceacc/spaceacc.github.io',
+            'https://github.com/spaceacc/spaceacc.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
